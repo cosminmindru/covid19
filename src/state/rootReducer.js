d1ac430f55a6714543549963e9edb6c71e5bf7f3
cosminmindru <cosminmindrut@gmail.com>
2020-03-28 15:05:03 +0000

    wip

--- src/state/rootReducer.js
+++ src/state/rootReducer.js
@@ -1,11 +1,9 @@
 import { combineReducers } from "@reduxjs/toolkit";
 
 import uiReducer from "./modules/ui";
-import userLocationReducer from "./modules/userLocation";
 
 const rootReducer = combineReducers({
-  ui: uiReducer,
-  userLocation: userLocationReducer
+  ui: uiReducer
 });
 
 export default rootReducer;
