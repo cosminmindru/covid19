f316c87bdf897ad98063de487f7725c571ca719c
cosminmindru <cosminmindrut@gmail.com>
2020-04-02 00:05:10 +0100

    add basic country stats

--- src/components/CountryStatsWidget.js
+++ src/components/CountryStatsWidget.js
@@ -11,7 +11,10 @@ import {
   WidgetHeader,
   WidgetContent
 } from "../styles/components/Widget";
-import { getCountries } from "../libs/covid19";
+import { getCountries, getCountryDetails } from "../libs/covid19";
+import { formatNumber } from "../utils/formatNumber";
+import { transparentize } from "polished";
+import get from "lodash/get";
 
 const Content = styled(WidgetContent)`
   display: grid;
@@ -41,12 +44,11 @@ const CountrySelectSection = styled.section`
 
 const MapSection = styled.section`
   grid-area: map;
+  position: relative;
 `;
 
 const CountrySearchWrapper = styled.div`
   grid-area: country-search;
-  top: 0;
-  position: sticky;
 `;
 
 const CountryList = styled(List)`
@@ -55,15 +57,87 @@ const CountryList = styled(List)`
 `;
 
 const FakeMap = styled.div`
+  position: relative;
   width: 100%;
-  height: 100%;
-  background-color: #aaa;
+  height: 80%;
+  background-color: #eee;
+  background-image: url("https://i.picsum.photos/id/237/1200/600.jpg");
+  background-size: cover;
+  background-repeat: no-repeat;
+
+  &:after {
+    content: "";
+    position: absolute;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    height: 4rem;
+    background: linear-gradient(
+      0deg,
+      rgba(255, 255, 255, 1) 0%,
+      rgba(255, 255, 255, 0.7) 50%,
+      rgba(255, 255, 255, 0) 100%
+    );
+  }
+`;
+
+const CountryStatsOverlay = styled.section`
+  z-index: 1;
+  display: grid;
+  grid-template-columns: 1fr;
+  grid-template-rows: auto;
+  position: absolute;
+  bottom: 1rem;
+  left: 1rem;
+  right: 1rem;
+  padding: 0 1rem;
+  border-radius: 0.5rem;
+  background-color: ${(props) => props.theme.colors.white};
+  box-shadow: 0 0.15rem 0.15rem
+    ${(props) => transparentize(0.9, props.theme.colors.black)};
+
+  @media ${(props) => props.theme.breakpoints.tablet} {
+    grid-template-columns: repeat(3, 1fr);
+    grid-template-rows: 1fr;
+    padding: 1rem 0;
+  }
+`;
+
+const CountryStat = styled.article`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  justify-content: center;
+  padding: 1.5rem 0;
+  margin: 0;
+
+  &:not(:last-child) {
+    border-bottom: 1px solid ${(props) => props.theme.colors.grey};
+  }
+
+  @media ${(props) => props.theme.breakpoints.tablet} {
+    grid-template-columns: repeat(3, 1fr);
+    grid-template-rows: 1fr;
+    padding: 0 1.5rem;
+
+    &:not(:last-child) {
+      border-right: 1px solid ${(props) => props.theme.colors.grey};
+      border-bottom: 0;
+    }
+  }
 `;
 
 const CountryStatsWidget = () => {
   const [selectedCountry, setSelectedCountry] = useState(null);
 
   const countries = useQuery(["countries", {}], getCountries);
+  const country = useQuery(
+    selectedCountry && [
+      `${selectedCountry.iso2}`,
+      { countryCode: selectedCountry.iso2 }
+    ],
+    getCountryDetails
+  );
 
   return (
     <Widget>
@@ -109,6 +183,32 @@ const CountryStatsWidget = () => {
         </CountrySelectSection>
         <MapSection>
           <FakeMap />
+          <CountryStatsOverlay>
+            <CountryStat>
+              <Typography variant="h6" gutterBottom>
+                Confirmed
+              </Typography>
+              <Typography variant="h5">
+                {formatNumber({ value: get(country, "data.confirmed.value") })}
+              </Typography>
+            </CountryStat>
+            <CountryStat>
+              <Typography variant="h6" gutterBottom>
+                Recovered
+              </Typography>
+              <Typography variant="h5">
+                {formatNumber({ value: get(country, "data.recovered.value") })}
+              </Typography>
+            </CountryStat>
+            <CountryStat>
+              <Typography variant="h6" gutterBottom>
+                Deaths
+              </Typography>
+              <Typography variant="h5">
+                {formatNumber({ value: get(country, "data.deaths.value") })}
+              </Typography>
+            </CountryStat>
+          </CountryStatsOverlay>
         </MapSection>
       </Content>
     </Widget>
