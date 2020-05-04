cf01c78b6e805efbcab4c3080d3251be6c07ac4e
cosminmindru <cosminmindrut@gmail.com>
2020-05-04 23:47:32 +0100

    add basic header with color mode switch

--- src/router/layouts/Main.js
+++ src/router/layouts/Main.js
@@ -3,30 +3,22 @@ import styled from "styled-components/macro";
 import ReactGA from "react-ga";
 import { useLocation } from "react-router-dom";
 import config from "../../config";
-
 import Meta from "../../components/Meta";
+import Header from "../../components/Header";
+import Container from "../../design/components/Container";
 
-const Wrapper = styled.div`
+const SLayout = styled.div`
   display: flex;
-  justify-content: center;
+  flex-direction: column;
   background-color: ${(props) => props.theme.colors.background};
-
-  @media ${(props) => props.theme.breakpoints.desktop} {
-    flex-direction: row;
-  }
 `;
 
 const Content = styled.main`
-  display: flex;
-  justify-content: center;
-  width: 100%;
-  max-width: ${(props) => props.theme.sizes.containerMaxWidth};
   min-height: 100vh;
-  padding: 1rem;
+  padding: 1rem 0;
 
   @media ${(props) => props.theme.breakpoints.desktop} {
-    flex-direction: row;
-    padding: 2rem;
+    padding: 2rem 0;
   }
 `;
 
@@ -42,10 +34,13 @@ const MainLayout = ({ children, meta }) => {
   }, [pathname]);
 
   return (
-    <Wrapper>
+    <SLayout>
       <Meta {...meta} />
-      <Content>{children}</Content>
-    </Wrapper>
+      <Header />
+      <Content>
+        <Container>{children}</Container>
+      </Content>
+    </SLayout>
   );
 };
 
