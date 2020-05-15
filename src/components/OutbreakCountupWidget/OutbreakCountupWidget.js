0d9f0c2e682693bb63dd54ff991c092a0db62d03
cosminmindru <cosminmindrut@gmail.com>
2020-05-15 13:38:38 +0100

    add basic countup functionality and update formatNumber annotations

--- src/components/OutbreakCountupWidget/OutbreakCountupWidget.js
+++ src/components/OutbreakCountupWidget/OutbreakCountupWidget.js
@@ -1,13 +1,55 @@
-import React from "react";
+import React, { useState, useEffect, useCallback } from "react";
+import dayjs from "dayjs";
+import formatNumber from "../../utils/formatNumber";
 import Widget from "../../design/components/Widget";
 
 const OutbreakCountupWidget = () => {
+  const [days, setDays] = useState(0);
+  const [hours, setHours] = useState(0);
+  const [minutes, setMinutes] = useState(0);
+  const [seconds, setSeconds] = useState(0);
+
+  const formatTime = (value) =>
+    formatNumber({ value, options: { minimumIntegerDigits: 2 } });
+
+  const getTime = useCallback(() => {
+    const outbreakDay1 = dayjs("2019-12-01");
+    const outbreakStartHour = dayjs().set("hour", 0);
+    const outbreakStartMinute = dayjs().set("minute", 0);
+    const outbreakStartSecond = dayjs().set("second", 0);
+
+    const today = dayjs();
+    const daysSince = today.diff(outbreakDay1, "day");
+    const hoursSince = today.diff(outbreakStartHour, "hour");
+    const minutesSince = today.diff(outbreakStartMinute, "minute");
+    const secondsSince = today.diff(outbreakStartSecond, "second");
+
+    setDays(formatTime(daysSince));
+    setHours(formatTime(hoursSince));
+    setMinutes(formatTime(minutesSince));
+    setSeconds(formatTime(secondsSince));
+  }, []);
+
+  useEffect(() => {
+    // Sync the time every 1s
+    setInterval(getTime, 1000);
+
+    return () => {
+      // Remove the listener on component unmount
+      clearInterval(getTime);
+    };
+  }, [getTime]);
+
   return (
     <Widget>
       <Widget.Header>
         <Widget.Title>Outbreak started</Widget.Title>
       </Widget.Header>
-      <Widget.Content>{/* TODO */}</Widget.Content>
+      <Widget.Content>
+        <h3>
+          {days} : {hours} : {minutes}: {seconds}
+        </h3>
+      </Widget.Content>
     </Widget>
   );
 };
